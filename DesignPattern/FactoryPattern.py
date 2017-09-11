a38c2f3adf3e2913d5c36c0805eb55137925aed8
Avijit Das <aviit.das@covidien.com>
2017-09-11 16:57:10 -0700

    Implement Factory design pattern in Python

---
+++ DesignPattern/FactoryPattern.py
@@ -0,0 +1,32 @@
+class Shape:
+    def shape(self):
+        raise NotImplementedError("Not implemented for this type.")
+
+
+class Square(Shape):
+    def shape(self):
+        print("Square.. ")
+
+
+class Circle(Shape):
+    def shape(self):
+        print("Circle.. ")
+
+
+class ShapeFactory:
+
+    def getshape(self, type1):
+        if type1 is None:
+            print('No shape mentioned.. ')
+
+        if type1 is "Circle":
+            return Circle()
+
+        if type1 is "Square":
+            return Square()
+
+
+if __name__ == '__main__':
+    factory = ShapeFactory()
+    factory.getshape("Circle").shape()
+    factory.getshape("Square").shape()
